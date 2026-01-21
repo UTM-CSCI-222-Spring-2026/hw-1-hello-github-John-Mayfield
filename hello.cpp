14f37f223a2ce14b341255e76b5cc7ab947226dd
John Mayfield <jmayfi17@skyhawks.utm.edu>
2026-01-21 18:09:10 +0000

    Initial version - incorrect output

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 /**
  * @file hello.cpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author jmayfi17@John-Mayfield
+ * @brief Hello world git commands
  */
 #include <iostream>
 
@@ -9,5 +9,5 @@ using namespace std;
 
 int main()
 {
-    // YOUR CODE HERE
+    cout << "Hello, world!" << endl;
 }
